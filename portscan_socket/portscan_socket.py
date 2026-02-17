bf67ad2f65dc88395217c19fbfd4295e9fa66b00
Aurelien Ammeloot <aurelienammeloot@fife.ac.uk>
2026-02-17 10:47:35 +0000

    v1 of program

--- portscan_socket/portscan_socket.py
+++ portscan_socket/portscan_socket.py
@@ -9,6 +9,10 @@
 import socket
 import datetime # (for timestamp purpose)
 
+# For time issues - 21 to 80 for now 
+MIN_PORT = 21
+MAX_PORT = 80
+
 # For scope, create those variables now
 host_ip = ""
 dns_success = False
@@ -25,5 +29,27 @@ while not dns_success:
         print("Unable to resolve", hostname, "into an IP address.")
         dns_success = False
 
+# TODO timestamp
+
+# Main loop
+for port in range(MIN_PORT, MAX_PORT+1):
+    # Create a TCP/IPV4 socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(.5) # Gives up after time in seconds
+
+    # Try to connect to ip / port
+    try:
+        sock.connect((host_ip, port))
+        print(f"{host_ip} port {port}: open.")
+    except KeyboardInterrupt:
+        # User typed Ctrl-C and wants to end the program
+        print("Program ended on your request.")
+        exit
+    except: # Ignore other exceptions
+        pass
+    
+
 # Debug
-print(host_ip)
+# print(host_ip)
+
+
